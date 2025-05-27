ce7e27dac589c6635c76f764861e65c70f8bc9af
wujinhao <sdutwujinhao@gmail.com>
2025-05-27 18:36:55 +0800

    upd: 清理调试信息和相关配置文件

--- utils/genRandom.js
+++
@@ -1,5 +0,0 @@
-function getRandomInRange(N, M) {
-    return Math.floor(Math.random() * (M - N + 1)) + N;
-}
-
-export default getRandomInRange;
--- utils/sleep.js
+++
@@ -1,5 +0,0 @@
-const sleep = async (ms)=> {
-    return new Promise(resolve=>setTimeout(resolve, ms))
-}
-
-export default sleep;
